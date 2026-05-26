834680acae11506fea7fce6f09e105dbcf71a886
reycodes406 <reginarigi23@gmail.com>
2026-05-26 23:00:58 +0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 function refreshWeather(response){
+    
+    
     let temperatureElement=document.querySelector("#temperature");
     let temperature= response.data.temperature.current;
     let cityElement=document.querySelector("#city");
+
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let iconElement=document.querySelector("#icon");
+    let windspeedElement=document.querySelector("#wind-speed");
+    let timeElement=document.querySelector("#time");
+    let date=new Date(response.data.time *1000);
+
+    console.log(response.data.condition.description);
+
     cityElement.innerHTML=response.data.city;
+    descriptionElement.innerHTML=response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity + "%";
+    iconElement.innerHTML = `<img src="${response.data.condition.icon}" alt="weather icon"/>`;
     temperatureElement.innerHTML=Math.round(temperature);
+    windspeedElement.innerHTML=response.data.wind.speed +"km/h";
+    timeElement.innerHTML=formatDate(date);}
+
+ function formatDate(date){
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+
+    let days=["Sunday","Tuesday","wednesday","Thursday","Friday","Saturday"];
+    let day=days[date.getDay()];
+
+    if (minutes<10) {
+
+       minutes =0`${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+
+
 }
 
 
